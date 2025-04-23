173bfe43c316f5733a599f29c4de46eaa00f8245
Somesingman <Somesingman@users.noreply.github.com>
2025-04-22 20:22:57 -0700

    Add useEffect that will update the list of game studios based on filters and searchs.

--- ea-graveyard/src/App.tsx
+++ ea-graveyard/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
 
 import List from './components/List';
@@ -9,37 +9,32 @@ import { FilterType } from './types/Filter';
 import rawStudioData from './studios.json';
 
 function App() {
-  // const [count, setCount] = useState(0)
-  const [currentFilter, updateFilter] = useState<FilterType>(FilterType.ALL)
+  const [currentFilter, setFilter] = useState<FilterType>(FilterType.ALL)
+  const [searchTag, setSearchTag] = useState('');
 
-  let studioList: StudioObj[] = rawStudioData.map(studioData => new StudioObj(studioData));
- 
+  const rawStudioList = rawStudioData.map(studioData => new StudioObj(studioData))
+  let studioList = useRef<StudioObj[]>(rawStudioList);
+
+  useEffect(() => {
+    const filteredStudioList = currentFilter === FilterType.ALL ? rawStudioList :
+      rawStudioList.filter(studio => {
+        if (currentFilter == FilterType.EA) {
+          return studio.acquiredBy == 'EA';
+        } else if (currentFilter == FilterType.OTHER) {
+          return studio.acquiredBy != 'EA';
+        }
+        return true;
+      }) 
+    if (searchTag === '') {
+      studioList.current = filteredStudioList;
+    }
+  })
 
   return (
     <>
       <h1 className="text-6xl text-center p-10">EA Graveyard</h1>
       <div className="p-4 text-center"> Search and Filter placeholder </div>
-      <List studios={studioList} />
-      {/* <div className="flex justify-center">
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
+      <List studios={studioList.current} />
     </>
   )
 }
