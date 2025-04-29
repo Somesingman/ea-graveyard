c78e06d2a9d5bfba2e15c24eafaa77fa6d6588d6
Somesingman <Somesingman@users.noreply.github.com>
2025-04-29 00:37:55 -0700

    Update make to handle e2e testing. Setup docker compose to support e2e testing with a containerized playwright server.

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,12 @@ FROM node:23-slim
 RUN mkdir /app
 WORKDIR /app
 
+COPY ea-graveyard/package.json /app/
+
+RUN npm install -g npm@11.3.0
+
 ENV NODE_ENV=development
 EXPOSE 8080
-RUN npm install -g npm@11.3.0
+EXPOSE 9323
+RUN npm install
+CMD ["npm", "run","dev"]
--- make.bat
+++ make.bat
@@ -5,18 +5,51 @@ SET arg1=%1
 if "%1" EQU "build" CALL :Build
 if "%1" EQU "setup" CALL :Setup
 if "%1" EQU "shell" CALL :Shell
+if "%1" EQU "start" CALL :Start
+if "%1" EQU "stop" CALL :Stop
+if "%1" EQU "int-shell" CALL :E2E-shell
+if "%1" EQU "int-start" CALL :E2E-start
 GOTO :EOF
 
 :: Docker Commands (Windows Makefile)
 
-:Build
-docker build -t ea-graveyard:latest .
-EXIT /b
+:: Potentially OLD make commands
+@REM :Build
+@REM docker build -t ea-graveyard:latest .
+@REM EXIT /b
 
-:Setup
-docker run --rm -it --mount type=bind,src=%cd%,target=//app ea-graveyard:latest npm install
+@REM :Setup
+@REM docker run --rm -it --mount type=bind,src=%cd%/ea-graveyard,target=//app ea-graveyard:latest npm install
+@REM EXIT /b
+
+@REM :Shell
+@REM docker run --rm -p 8080:8080 -it --mount type=bind,src=%cd%/ea-graveyard,target=//app ea-graveyard:latest /bin/bash
+@REM EXIT /b
+
+@REM :E2E
+@REM docker run -p 3000:3000 -p 9323:9323 --rm --network host --init -it --workdir /home/pwuser --user pwuser mcr.microsoft.com/playwright:v1.52.0-noble /bin/sh -c "npx -y playwright@1.52.0 run-server --port 3000 --host 0.0.0.0"
+@REM EXIT /b
+
+:Build
+docker compose build
 EXIT /b
 
 :Shell
-docker run --rm -p 8080:8080 -it --mount type=bind,src=%cd%,target=//app ea-graveyard:latest /bin/bash
+docker exec -it ea-graveyard-website-1 /bin/bash
+EXIT /b
+
+:E2E-shell
+docker exec -it ea-graveyard-playwright-1 /bin/bash
+EXIT /b
+
+:Start
+docker compose up --remove-orphans --detach 
+EXIT /b
+
+:Stop
+docker compose --profile "*" down --remove-orphans
+EXIT /b
+
+:E2E-start
+docker compose --profile e2e up --detach
 EXIT /b
