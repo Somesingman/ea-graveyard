32dfe44bd25a71d6b50d8ac47dd2c83513b9fb4f
Somesingman <Somesingman@users.noreply.github.com>
2025-05-02 17:40:04 -0700

    Make deploy workflow run after tests workflow completes.

--- ea-graveyard/src/components/List.tsx
+++ ea-graveyard/src/components/List.tsx
@@ -7,7 +7,7 @@ interface ListProps {
 }
 
 function List({studios}: ListProps) {
-
+    
     return (
         <ul id="studioList" className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-2 mx-auto">
             {studios.map(studio => (
