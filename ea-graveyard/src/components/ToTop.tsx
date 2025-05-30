05f94ce7be99e147f013deb62486110beff87ad8
Somesingman <Somesingman@users.noreply.github.com>
2025-05-30 00:10:46 -0700

    Add hover effects to back to top button.

--- ea-graveyard/src/components/ToTop.tsx
+++ ea-graveyard/src/components/ToTop.tsx
@@ -8,7 +8,7 @@ interface toTopProps {
 function ToTop({isVisible, scrollHandler}: toTopProps) {
     return (
         <button className={`${isVisible ? "" : "hidden"}  block rounded-full fixed bottom-20 right-20 z-10 cursor-pointer`} onClick={() => scrollHandler()}>
-            <ArrowUpCircleIcon className="sm:w-[40px] lg:w-[80px] rounded-full bg-white dark:bg-gray-800 fill-gray-800 dark:fill-white shadow-lg" />
+            <ArrowUpCircleIcon className="sm:w-[40px] lg:w-[80px] rounded-full bg-white dark:bg-gray-800 fill-gray-800 hover:fill-gray-600 dark:fill-white dark:hover:fill-gray-200 shadow-lg" />
         </button>
     )
 }
